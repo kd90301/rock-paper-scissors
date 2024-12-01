4b767f3096abd90279e90ae5dbc42e08789c17df
kd90301 <kyle.p.darling@gmail.com>
2024-12-01 15:18:18 -0800

    Add randomized computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Rock, Paper, Scissors")
+// console.log("Rock, Paper, Scissors")
+
+function getComputerChoice(){
+  // let options = ("rock", "paper", "scissors");
+
+  let randomChoice = Math.random()
+  let computerChoice = "";
+  if (randomChoice <= .333) {
+    computerChoice = "Rock";
+  }
+  else if (randomChoice >= .666) {
+    computerChoice = "Scissors"
+  }
+  else {
+    computerChoice = "Paper"
+  }
+  console.log(computerChoice)
+  
+}
+
+getComputerChoice()
