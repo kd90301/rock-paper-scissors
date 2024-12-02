0b76565bdde4c675af26325ad00c59ad8d1263dc
kd90301 <kyle.p.darling@gmail.com>
2024-12-01 19:00:29 -0800

    Add basic play round functionality results log to console, score functionality to be implemented

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 // console.log("Rock, Paper, Scissors")
 
+
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
   // let options = ("rock", "paper", "scissors"); oh, right - this isn't Python
 
-  let randomChoice = Math.random()
+  let randomChoice = Math.random();
   let computerChoice = "";
   if (randomChoice <= .333) {
     computerChoice = "rock";
   }
   else if (randomChoice >= .666) {
-    computerChoice = "scissors"
+    computerChoice = "scissors";
   }
   else {
-    computerChoice = "paper"
+    computerChoice = "paper";
   }
   // console.log(computerChoice) 
+  return computerChoice ;
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Please select rock, paper or scissors. Lowercase answers only.")
-  console.log(humanChoice)
+  let humanChoice = prompt("Please select rock, paper or scissors.");
+  let caseInsensitiveChoice = humanChoice.toLowerCase();
+  return caseInsensitiveChoice ;
+  // console.log(humanChoice)
 }
 
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
 
-getComputerChoice();
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("It\'s a tie!");
+  } 
+  else if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("Rock is covered by paper! Computer gets 1 point.");
+  }
+  else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("Rock breaks scissors! You get 1 point.");
+  }
+  else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("Paper covers rock! You get 1 point.");
+  }
+  else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("Paper is cut by scissors! Computer gets 1 point.");
+  }
+  else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("Scissors cut paper! You get 1 point");
+  }
+  else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("Scissors are broken by rock! Computer gets 1 point.");
+  }
+  else {
+    console.log("it\'s ... not a tie ...");
+  }
+  
+} 
+
+
+console.log(computerSelection);
+console.log(humanSelection);
+
+playRound(humanSelection, computerSelection);
